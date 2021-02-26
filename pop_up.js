56e61fa99bebd8241dc3aa45dcaf5b11c687bdcb
pushpendrakumar <pkc3766@gmail.com>
2021-02-26 23:52:34 +0530

    removed extra commented code

--- pop_up.js
+++ pop_up.js
@@ -1,68 +1,3 @@
-// $('a').click(function() {
-//     $('#sites a').removeClass('focus');
-//     $(this).find('a').addClass('focus');
-//   });
-
-//   document.querySelectorAll("a").addEventListener("click", function(){
-//       document.querySelectorAll("a").addClass('focus');
-
-//   });
-// function removeFocusFromAll()
-// {
-//     var sites=document.querySelectorAll("i");
-//     for(var i=0;i<sites.length;i++)
-//     {
-//         sites[i].classList.remove('focus');
-//     }
-    
-// }
-// document.querySelectorAll("i")[0].classList.add('focus');
-// document.getElementById("youtube").addEventListener("click", function() {
-//     removeFocusFromAll();
-//     document.querySelectorAll("i")[0].classList.add('focus');
-//   });
-//   document.getElementById("facebook").addEventListener("click", function() {
-//     removeFocusFromAll();
-//     document.querySelectorAll("i")[1].classList.add('focus');
-
-//   });
-//   document.getElementById("instagram").addEventListener("click", function() {
-//     removeFocusFromAll();
-//     document.querySelectorAll("i")[2].classList.add('focus');
-//   });
-//   document.getElementById("mail").addEventListener("click", function() {
-//     removeFocusFromAll();
-//     document.querySelectorAll("i")[3].classList.add('focus');
-//   });
-
-
-// window.addEventListener('DOMContentLoaded', () => {
-  // ...query for the active tab...
-  
-// });
-
-// var port = chrome.extension.connect({
-//   name: "Sample Communication"
-// });
-// port.postMessage("Hi BackGround");
-// port.onMessage.addListener(function(msg) {
-//   console.log("message recieved" + msg);
-//   document.getElementById("timeSpent").textContent=msg;
-// });
-//document.getElementById("timeSpent").textContent="hello there";
-// chrome.runtime.onMessage.addListener(
-//   function(msg, sender, sendResponse) {
-//     document.getElementById("timeSpent").textContent="hello there";
-//   }
-// );
-// function getStorageKeyValue(key, onGetStorageKeyValue){
-//   chrome.storage.local.get([key], function(result) {
-//      onGetStorageKeyValue(result.key);
-//   });
-// }
-
-// document.getElementById("startTimer").disabled=true;
-
 function sendMessageToContentScript()
 {
   chrome.tabs.query({
@@ -72,8 +7,6 @@ function sendMessageToContentScript()
         chrome.tabs.sendMessage(
         tabs[0].id,{from: 'pop_up', subject: 'stopTimer'});
   });
-  // var port = chrome.runtime.connect({name: "Timer"});
-  // port.postMessage({info: "stopTimer"});
 }
 document.getElementById("getTimeSpent").addEventListener("click",function(){
   chrome.storage.local.get(["timeSpent"], function(result) {
